8f385f196d4c0998022ddc3ae9ab5dd6abe4e479
Tangerinez <gtang159@gmail.com>
2019-04-03 20:59:24 -0700

    Combined all the html docs onto FirstScreen html... Working to fix attack button functionality, which is the last thing to fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -50,7 +50,7 @@ function initializeCharAttack(obj) {
 
 // If player is alive or not
 function alive(obj) {
-    if (obj.healthPoints > 0) {
+    if (obj.health > 0) {
         return true;
     };
     return false;
@@ -83,56 +83,39 @@ function charCards(gameID) {
 };
 
 //Updates the pictures after you pick character cards
-function updateCards(gameDiv, enemiesLeftDiv) {
+function updateCards(gameDiv, enemiesLeft) {
     $(gameDiv).children().remove();
     for (var i = 0; i < charArray.length; i++) {
-        $(enemiesLeftDiv).append("<img />");
-        $(enemiesLeftDiv + " img:last-child").attr("id", charArray[i].name);
-        $(enemiesLeftDiv + " img:last-child").attr("src", charArray[i].pic);
-        $(enemiesLeftDiv + " img:last-child").attr("width", 150);
-        $(enemiesLeftDiv + " img:last-child").addClass("img-thumbnail");
+        $(enemiesLeft).append("<img />");
+        $(enemiesLeft + " img:last-child").attr("id", charArray[i].name);
+        $(enemiesLeft + " img:last-child").attr("src", charArray[i].image);
+        $(enemiesLeft + " img:last-child").attr("width", 150);
+        $(enemiesLeft + " img:last-child").addClass("img-thumbnail");
     }
 }
 
 //Change from first screen to second screen
 function firstTosecond() {
-    $("#first").empty();
+    $("#first").hide();
     $("#second").show();
 };
 
 //Change from second screen to win screen
 function secondTowin() {
-    $("#second").empty();
-    $("#win-screen").show();
+    $("#second").hide();
+    $(".win-screen").show();
 };
 
 //Change from second screen to lose screen
-function secondTowin() {
-    $("#second").empty();
-    $("#lose-screen").show();
+function secondTolose() {
+    $("#second").hide();
+    $(".lose-screen").show();
 };
 
 
 /////////////////////* ON-CLICK FUNCTIONALITY */////////////////////////
 
 $(document).on("click", "img", function() {
-    console.log("onclick");
-    if (!charSelected) {     // If the user has NOT selected a character
-        for (var i = 0; i< charArray.length; i++) {   // For each character in the array...
-            if (charArray[i].name === (this).id) {  // If that character's name is the same as the name that was set for it's id...
-                player = charArray[i]; // Player object becomes that Character
-                initializeCharAttack(player); // Generate the user's attack power
-                charArray.splice(i,1);  // Removes that character from the array
-                charSelected = true;  
-                window.location.href = "SecondScreen.html";
-                $("#message").text("Choose your foe."); // PROBLEM -> Code is not rendering because this class is from another html DOC!
-            };
-        };
-        updateCards("#game", "#enemiesLeft");       
-        $("#player").append(this); // appends the selected player to the div
-        $("#player").append(player.name);    // appends player name to the player id div
-        $("#playerHealth").append("HP: " + player.health); //
-    };
     if (charSelected && !enemySelected && (this.id != player.name)) {   // If user HAS selected character, enemy has NOT been selected, AND id matches with character's name
         for (var x = 0; x<charArray.length; x++) {  // For every element in char array
             if (charArray[x].name === (this).id) {      
@@ -144,37 +127,53 @@ $(document).on("click", "img", function() {
         };
         $("#enemyDiv").append(this); // appends the selected defender to the div 
         $("#enemyDiv").append("<br>" + enemy.name);  // appends enemy name to the div
-        $("#enemyHealth").append("HP: " + enemy.healthPoints);
+        $("#enemyHealth").append("HP: " + enemy.health);
+    };
+    if (!charSelected) {     // If the user has NOT selected a character
+        for (var i = 0; i< charArray.length; i++) {   // For each character in the array...
+            if (charArray[i].name === (this).id) {  // If that character's name is the same as the name that was set for it's id...
+                player = charArray[i]; // Player object becomes that Character
+                initializeCharAttack(player); // Generate the user's attack power
+                charArray.splice(i,1);  // Removes that character from the array
+                charSelected = true;  
+                firstTosecond(); // transition from first to second page
+                $("#message").text("Choose your foe."); // PROBLEM -> Code is not rendering because this class is from another html DOC!
+            };
+        };
+        updateCards("#game", "#enemiesLeft");       
+        $("#player").append(this); // appends the selected player to the div
+        $("#player").append(player.name);    // appends player name to the player id div
+        $("#playerHealth").append("HP: " + player.health); //
     };
 });
 
 // Attack Button Onclick Event
 $(document).on("click", "#attackBtn", function() {
-    if(charSelected && enemySelected) {
-        if (alive(player) && alive(enemy)) {
-            player.attack(enemy);
-            enemy.returnDmg(player);
-            $("#playerHealth").html("Health: " + player.healthPoints);
-            $("#enemyHealth").html("Health: " + defender.healthPoints);
-            if (!alive(enemy)) {
-                $("#playerHealth").html("You have defeated the enemy.");
-                $("#message").html("Pick another enemy to battle!");
+    if(charSelected && enemySelected) {    // if the player and enemy has been selected
+        if (alive(player) && alive(enemy)) {       // if player and enemy are BOTH alive
+            player.attack(enemy);        // Player attacks the enemy
+            enemy.returnDmg(player);      // Enemy returns damage onto the player
+            $("#playerHealth").html("Health: " + player.health);       // Underneath player, new HP shows up
+            $("#enemyHealth").html("Health: " + defender.health);      // Underneath enemy, new HP shows up
+            if (!alive(enemy)) {       // if the enemy is not alive
+                $("#playerHealth").html("You have defeated the enemy.");      // display under the player that you have defeated the enemy
+                $("#message").html("Pick another enemy to battle!");       // display under the player that you can now pick another enemy to battle
             }
-            if (!isAlive(player)) {
-                $("#playerHealth").html("You have been defeated.");
-                window.location.href = "LoseScreen.html";
+            if (!isAlive(player)) {     // if the player is not alive
+                $("#playerHealth").html("You have been defeated.");     // player has lost the game
+                secondTolose();    // shifts to lose screen
                 $(document).on("click", "#attackBtn", function () { // restarts game
-                    /// restart game here
+                    location.reload();
                 });
             }
         }
-        if (!alive(enemy)) {
-            $("#enemyDiv").children().remove();
-            $("#enemyDiv").html("");
+        if (!alive(enemy)) {     //if there are no more enemies alive
+            $("#enemyDiv").children().remove();     // remove all child elements from enemy div
+            $("#enemyDiv").html("");      
             $("#enemyHealth").html("");
-            enemySelected = false;
-            if (winOrlose()) {
-                window.location.href = "WinScreen.html";
+            enemySelected = false;       // no enemy selected
+            if (winOrlose()) {    // if winOrlose = true
+                secondTowin();     // shift to win screen
             };
         };
     };
@@ -182,6 +181,9 @@ $(document).on("click", "#attackBtn", function() {
 
 // Runs when you load the webpage
 $(document).ready(function () {
+    $("#second").hide();
+    $(".win-screen").hide();
+    $(".lose-screen").hide();
     charInitialize();
     charCards("#game");
 });
